92d37ca868dc587ab116718fd5f340919e8e555b
Harout Mardirossian <harmardir@users.noreply.github.com>
2022-08-06 13:42:42 +0300

    adding views and URLs mapping

--- globomantics/store/views.py
+++ globomantics/store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello there, Globomantics E-commerce store front coming here ...")
+
+
+def detail(request):
+    return HttpResponse("Hello there, Globomantics E-commerce store front coming here ...")
+
+
+def electronics(request):
+    return HttpResponse("Hello there, Globomantics E-commerce store front coming here ...")
+
